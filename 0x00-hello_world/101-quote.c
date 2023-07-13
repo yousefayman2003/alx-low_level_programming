f2f3cd32e10c0c7efa6d04fd80e71a49b59e2ef9
yousefayman2003 <yousefayman20003@gmail.com>
2023-07-13 10:58:43 -0700

    uploaded advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print a statment to the standard error
+ *
+ * Return: 1 --> int (Error Massege)
+*/
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n");
+return (1);
+}
