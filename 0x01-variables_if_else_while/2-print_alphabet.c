e1deed8a26ca8f315e251a37e6c5ba385b9a0df6
yousefayman2003 <yousefayman20003@gmail.com>
2023-07-14 05:00:36 -0700

    task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabets
+ *
+ * Return: Always 0 if success else fail
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+
+	return (0);
+}
