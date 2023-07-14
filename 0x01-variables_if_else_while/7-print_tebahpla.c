7446e49667309ffd5f8ab56350f3bb5578fd0e19
yousefayman2003 <yousefayman20003@gmail.com>
2023-07-14 05:37:44 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints alphabets
+ *
+ * Return: Always 0 if success else fail
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
