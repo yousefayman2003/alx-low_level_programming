b3cbf424423c54428c9de7b33c6ffb334b982b8e
yousefayman2003 <yousefayman20003@gmail.com>
2023-07-14 06:38:42 -0700

    adv 3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints numbers
+ *
+ * Return: Always 0 if success else fail
+*/
+
+int main(void)
+{
+	int firstDigit = 0, secondDigit;
+
+	while (firsrDigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit % 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondDigit++;
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
