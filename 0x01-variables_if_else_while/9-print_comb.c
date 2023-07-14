3469b96fd7c27d7c9efb3ecbfcd6b129c401e192
yousefayman2003 <yousefayman20003@gmail.com>
2023-07-14 05:53:39 -0700

    task10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a number
+ *
+ * Return: Always 0 if success else fail
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n != 10)
+	{
+		putchar(n + '48');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
