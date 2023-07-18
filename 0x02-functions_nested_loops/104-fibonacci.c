cd98a2e3cc0f8349ea5d9528559f4a20829f0466
yousefayman2003 <yousefayman20003@gmail.com>
2023-07-18 07:09:20 -0700

    adv 5

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints first 98 fib nums
+ *			starting with 1 and 2 followed by a new line
+ *
+ * Return: Always 0 if success else fail
+*/
+int main(void)
+{
+	int count, initial;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (count = 1; count <= 98; count++)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 &&  initial > 0)
+		{
+			printf("%d", 0;)
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
