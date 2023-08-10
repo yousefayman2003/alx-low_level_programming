de58f60557b2cc3283b16200cc6a4fbd0b15e180
yousefayman2003 <yousefayman20003@gmail.com>
2023-08-10 05:45:31 -0700

    upload

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory using malloc
- * @b: int
- * Return: pointer to the intialized array or NULL
-*/
+ * Description: malloc_checked - function allocats memory using malloc
+ * Input:
+ *	@b: [int] --> number of bytes what you want to reverse in memory
+ * Return: if success return pointer to the first byte of reserved memory
+ * otherwise return an exit function with status 98
+ */
 
 void *malloc_checked(unsigned int b)
 {
-	void *m;
+	void *p;
 
-	m = malloc(b);
-	if (m == NULL)
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
 	else
-		return (m);
+		return (p);
 }
