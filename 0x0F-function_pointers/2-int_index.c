72f175f924b2ed48c7a4b1cd33892a8284678cbd
yousefayman2003 <yousefayman20003@gmail.com>
2023-08-16 09:05:24 -0700

    up

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array && size && cmp)
-	{
+	if (array && size && cmp)	
 		while (i < size)
 		{
 			if (cmp(array[i]))
 				return (i);
 			i++;
 		}
-	}
 	return (-1)
 }
