333824887e8a92207f31e760eb5bbfc445038683
yousefayman2003 <yousefayman20003@gmail.com>
2023-08-16 10:01:55 -0700

    up

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include "function_pointer.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: arg count
+ * @argv: arg list
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (arg != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
