6bb579c08a9ba288a5c48e957516e08bafca4b4b
yousefayman2003 <yousefayman20003@gmail.com>
2023-08-16 13:44:31 -0700

    up

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC
-#define CALC
+#ifndef CALC_H
+#define CALC_H
 
 #include <stdlib.h>
 #include <stdio.h>
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * get_op_func - gets pointer to right op func
