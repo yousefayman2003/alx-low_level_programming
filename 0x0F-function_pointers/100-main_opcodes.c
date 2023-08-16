f5148275001da8139a5cfbd6c9984958482820e0
yousefayman2003 <yousefayman20003@gmail.com>
2023-08-16 13:53:04 -0700

    upload

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 	char *p = (char *)main;
 	int b;
 
-	if (arg != 2)
+	if (argc != 2)
 		printf("Error\n"), exit(1);
 	b = atoi(argv[1]);
 	if (b < 0)
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -8,12 +8,12 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
-	i++;
+		i++;
 	}
 
 	return (NULL);
