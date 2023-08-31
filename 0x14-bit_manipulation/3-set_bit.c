9e5384a67ebd2e14d90db96a267e33e29e01b177
yousefayman <yousefayman20003@gmail.com>
2023-08-31 05:02:39 -0700

    upload

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,9 +8,7 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int size = sizeof(n) * 8;
-
-	if (index >= size)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 
 	return (!!(*n != 1L << index));
