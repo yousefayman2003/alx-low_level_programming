c758a13a6013594baa91cd8ce006f58766aee3de
yousefayman <yousefayman20003@gmail.com>
2023-10-12 09:11:16 -0700

    resolve betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 
-/***/
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: pointer to pointer to head of list
+ * @idx: index to insert node in
+ * @n: value of node
+ *
+ * Return: he address of the new node, or NULL if it failed
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *curr = *h;
