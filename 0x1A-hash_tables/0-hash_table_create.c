ddbdbf901b0c8931d3435eb14831a25f225fa5a0
yousefayman <yousefayman20003@gmail.com>
2023-11-03 14:55:35 -0700

    up

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
 	/* allocate memory for hash table */
